177631906a3ccd4fbfabb67fa214ca5826fb35ef
Aniruddha Mukherjee <aniruddha.xccentric@gmail.com>
2020-06-09 20:43:10 +0530

    Updates

---
+++ precache-manifest.49e2e182d4923c255717ba76ad9e0bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58b486563d1561eb89d53635aa80c42",
+    "url": "/weather-pwa/index.html"
+  },
+  {
+    "revision": "22e1817a5766150d91d8",
+    "url": "/weather-pwa/static/css/main.15fb3d4c.chunk.css"
+  },
+  {
+    "revision": "9e47500990cac29f09c3",
+    "url": "/weather-pwa/static/js/2.39006cd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-pwa/static/js/2.39006cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e1817a5766150d91d8",
+    "url": "/weather-pwa/static/js/main.e1c8d959.chunk.js"
+  },
+  {
+    "revision": "9a2552042856dd06bf63",
+    "url": "/weather-pwa/static/js/runtime-main.55fd93aa.js"
+  }
+]);
